c41b4b61b87cc36a9fb9480419e879e2fb2647de
Venkatesh-Arumugam <144714314+Venkatesh-Arumugam@users.noreply.github.com>
2025-12-06 16:15:36 -0500

    Refactor DCT implementation for improved accuracy

--- hls/dct_accel.cpp
+++ hls/dct_accel.cpp
@@ -2,62 +2,61 @@
 #include <ap_fixed.h>
 #include <hls_math.h>
 
-typedef ap_uint<8>   pixel_t;
-typedef ap_fixed<18,4> dct_t;   // internal
-typedef ap_int<16>   coeff_t;   // stored DCT coefficient
+typedef ap_uint<8>    pixel_t;
+typedef ap_fixed<24,6> dct_t;   
+typedef ap_int<18>    coeff_t;
 
 static const int N = 8;
 
-// Orthonormal DCT matrix
 static const dct_t C[N][N] = {
-    {0.353553f, 0.353553f, 0.353553f, 0.353553f, 0.353553f, 0.353553f, 0.353553f, 0.353553f},
-    {0.490393f, 0.415735f, 0.277785f, 0.097545f,-0.097545f,-0.277785f,-0.415735f,-0.490393f},
-    {0.461940f, 0.191342f,-0.191342f,-0.461940f,-0.461940f,-0.191342f, 0.191342f, 0.461940f},
-    {0.415735f,-0.097545f,-0.490393f,-0.277785f, 0.277785f, 0.490393f, 0.097545f,-0.415735f},
-    {0.353553f,-0.353553f,-0.353553f, 0.353553f, 0.353553f,-0.353553f,-0.353553f, 0.353553f},
-    {0.277785f,-0.490393f, 0.097545f, 0.415735f,-0.415735f,-0.097545f, 0.490393f,-0.277785f},
-    {0.191342f,-0.461940f, 0.461940f,-0.191342f,-0.191342f, 0.461940f,-0.461940f, 0.191342f},
-    {0.097545f,-0.277785f, 0.415735f,-0.490393f, 0.490393f,-0.415735f, 0.277785f,-0.097545f}
+    {0.353553, 0.353553, 0.353553, 0.353553, 0.353553, 0.353553, 0.353553, 0.353553},
+    {0.490393, 0.415735, 0.277785, 0.097545,-0.097545,-0.277785,-0.415735,-0.490393},
+    {0.461940, 0.191342,-0.191342,-0.461940,-0.461940,-0.191342, 0.191342, 0.461940},
+    {0.415735,-0.097545,-0.490393,-0.277785, 0.277785, 0.490393, 0.097545,-0.415735},
+    {0.353553,-0.353553,-0.353553, 0.353553, 0.353553,-0.353553,-0.353553, 0.353553},
+    {0.277785,-0.490393, 0.097545, 0.415735,-0.415735,-0.097545, 0.490393,-0.277785},
+    {0.191342,-0.461940, 0.461940,-0.191342,-0.191342, 0.461940,-0.461940, 0.191342},
+    {0.097545,-0.277785, 0.415735,-0.490393, 0.490393,-0.415735, 0.277785,-0.097545}
 };
 
-// 2D DCT on 8x8 block, output raw coefficients
-static void dct_2d(pixel_t in_blk[8][8], coeff_t coef[8][8]) {
+static void dct_2d(pixel_t in_blk[8][8], coeff_t out_blk[8][8]) {
 #pragma HLS INLINE
 
     dct_t tmp[8][8];
 #pragma HLS ARRAY_PARTITION variable=tmp complete dim=0
 
-    // Row transform: tmp[u][v] = sum_x C[u][x] * (in[x][v] - 128)
-    ROWS:
-    for (int u = 0; u < N; u++) {
+    // --------------------------
+    // Correct Row Transform
+    // F(u,v) = sum_x C[u][x] * (in[v][x] - 128)
+    // --------------------------
+    for (int u = 0; u < 8; u++) {
 #pragma HLS UNROLL
-        for (int v = 0; v < N; v++) {
+        for (int v = 0; v < 8; v++) {
 #pragma HLS UNROLL
             dct_t acc = 0;
-            for (int x = 0; x < N; x++) {
+            for (int x = 0; x < 8; x++) {
 #pragma HLS UNROLL
-                acc += C[u][x] * (dct_t)((int)in_blk[x][v] - 128);
+                acc += C[u][x] * (dct_t)((int)in_blk[v][x] - 128);
             }
             tmp[u][v] = acc;
         }
     }
 
-    // Column transform: F[u][v] = sum_y tmp[u][y] * C[v][y]
-    COLS:
-    for (int u = 0; u < N; u++) {
+    // --------------------------
+    // Correct Column Transform
+    // F(u,v) = sum_y tmp[u][y] * C[v][y]
+    // --------------------------
+    for (int u = 0; u < 8; u++) {
 #pragma HLS UNROLL
-        for (int v = 0; v < N; v++) {
+        for (int v = 0; v < 8; v++) {
 #pragma HLS UNROLL
             dct_t acc = 0;
-            for (int y = 0; y < N; y++) {
+            for (int y = 0; y < 8; y++) {
 #pragma HLS UNROLL
                 acc += tmp[u][y] * C[v][y];
             }
-            // store rounded coefficient as 16-bit signed
             int val = (int)hls::round(acc);
-            if (val < -32768) val = -32768;
-            if (val >  32767) val =  32767;
-            coef[u][v] = (coeff_t)val;
+            out_blk[u][v] = (coeff_t)val;
         }
     }
 }
@@ -72,23 +71,6 @@ extern "C" void dct_accel(
     int width,
     int height
 ) {
-#pragma HLS INTERFACE m_axi port=inR  offset=slave bundle=gmem0
-#pragma HLS INTERFACE m_axi port=inG  offset=slave bundle=gmem1
-#pragma HLS INTERFACE m_axi port=inB  offset=slave bundle=gmem2
-#pragma HLS INTERFACE m_axi port=outR offset=slave bundle=gmem3
-#pragma HLS INTERFACE m_axi port=outG offset=slave bundle=gmem4
-#pragma HLS INTERFACE m_axi port=outB offset=slave bundle=gmem5
-
-#pragma HLS INTERFACE s_axilite port=inR    bundle=control
-#pragma HLS INTERFACE s_axilite port=inG    bundle=control
-#pragma HLS INTERFACE s_axilite port=inB    bundle=control
-#pragma HLS INTERFACE s_axilite port=outR   bundle=control
-#pragma HLS INTERFACE s_axilite port=outG   bundle=control
-#pragma HLS INTERFACE s_axilite port=outB   bundle=control
-#pragma HLS INTERFACE s_axilite port=width  bundle=control
-#pragma HLS INTERFACE s_axilite port=height bundle=control
-#pragma HLS INTERFACE s_axilite port=return bundle=control
-
     pixel_t R_blk[8][8], G_blk[8][8], B_blk[8][8];
 #pragma HLS ARRAY_PARTITION variable=R_blk complete dim=0
 #pragma HLS ARRAY_PARTITION variable=G_blk complete dim=0
@@ -99,22 +81,18 @@ extern "C" void dct_accel(
 #pragma HLS ARRAY_PARTITION variable=G_coef complete dim=0
 #pragma HLS ARRAY_PARTITION variable=B_coef complete dim=0
 
-    // Process in 8x8 tiles
     for (int by = 0; by < height; by += 8) {
         for (int bx = 0; bx < width; bx += 8) {
 
             // Load block
             for (int y = 0; y < 8; y++) {
                 for (int x = 0; x < 8; x++) {
-#pragma HLS UNROLL
                     int gx = bx + x;
                     int gy = by + y;
-                    int idx = gy * width + gx;
-
                     if (gx < width && gy < height) {
-                        R_blk[y][x] = inR[idx];
-                        G_blk[y][x] = inG[idx];
-                        B_blk[y][x] = inB[idx];
+                        R_blk[y][x] = inR[gy * width + gx];
+                        G_blk[y][x] = inG[gy * width + gx];
+                        B_blk[y][x] = inB[gy * width + gx];
                     } else {
                         R_blk[y][x] = 0;
                         G_blk[y][x] = 0;
@@ -123,20 +101,17 @@ extern "C" void dct_accel(
                 }
             }
 
-            // DCT
             dct_2d(R_blk, R_coef);
             dct_2d(G_blk, G_coef);
             dct_2d(B_blk, B_coef);
 
-            // Store coefficients (one per pixel position in block)
+            // Store block
             for (int y = 0; y < 8; y++) {
                 for (int x = 0; x < 8; x++) {
-#pragma HLS UNROLL
                     int gx = bx + x;
                     int gy = by + y;
-                    int idx = gy * width + gx;
-
                     if (gx < width && gy < height) {
+                        int idx = gy * width + gx;
                         outR[idx] = R_coef[y][x];
                         outG[idx] = G_coef[y][x];
                         outB[idx] = B_coef[y][x];
