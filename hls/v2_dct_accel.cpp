a2a0fc43ae4f44a873fc195a2ed1730c86417426
Venkatesh-Arumugam <144714314+Venkatesh-Arumugam@users.noreply.github.com>
2025-12-11 19:38:27 -0500

    Refactor DCT acceleration for fixed dimensions
    Updated DCT acceleration implementation to use fixed dimensions for width and height, optimizing performance and resource usage.

--- hls/v2_dct_accel.cpp
+++ hls/v2_dct_accel.cpp
@@ -1,8 +1,8 @@
 /******************************************************************************
- * CONFIGURATION 2: PIPELINED
- * File: dct_config2_pipelined.cpp
- * Description: Add PIPELINE II=1 to BLOCK_X loop
- * Expected: 2-3x speedup, moderate resource increase
+ * VERSION 2: HARDCODED DIMENSIONS WITH COMPATIBLE INTERFACE
+ * File: v2_dct_accel_fixed.cpp
+ * Description: Fixed 1920x1080 with width/height params for host compatibility
+ * Expected: 3-4x speedup, very high resource usage
  ******************************************************************************/
 
 #include <ap_int.h>
@@ -13,9 +13,10 @@ typedef ap_uint<8>  pixel_t;
 typedef ap_int<16>  coeff_t;
 typedef ap_fixed<24,12> dct_t;
 
-static const int N = 8;
+#define WIDTH 1920
+#define HEIGHT 1080
 
-static const dct_t C[N][N] = {
+static const dct_t C[8][8] = {
     {0.353553, 0.353553, 0.353553, 0.353553, 0.353553, 0.353553, 0.353553, 0.353553},
     {0.490393, 0.415735, 0.277785, 0.097545,-0.097545,-0.277785,-0.415735,-0.490393},
     {0.461940, 0.191342,-0.191342,-0.461940,-0.461940,-0.191342, 0.191342, 0.461940},
@@ -26,9 +27,9 @@ static const dct_t C[N][N] = {
     {0.097545,-0.277785, 0.415735,-0.490393, 0.490393,-0.415735, 0.277785,-0.097545}
 };
 
-static void dct_2d(pixel_t in_blk[8][8], coeff_t out_blk[8][8])
+static void dct_2d(pixel_t in[8][8], coeff_t out[8][8])
 {
-#pragma HLS INLINE
+#pragma HLS INLINE off
     dct_t tmp[8][8];
 #pragma HLS ARRAY_PARTITION variable=tmp complete dim=0
 
@@ -39,7 +40,7 @@ static void dct_2d(pixel_t in_blk[8][8], coeff_t out_blk[8][8])
             dct_t acc = 0;
             for (int x = 0; x < 8; x++) {
 #pragma HLS UNROLL
-                acc += C[u][x] * (dct_t)((int)in_blk[x][v] - 128);
+                acc += C[u][x] * (dct_t)((int)in[x][v] - 128);
             }
             tmp[u][v] = acc;
         }
@@ -57,7 +58,7 @@ static void dct_2d(pixel_t in_blk[8][8], coeff_t out_blk[8][8])
             int val = (int)hls::round(acc);
             if (val < -32768) val = -32768;
             if (val >  32767) val =  32767;
-            out_blk[u][v] = (coeff_t)val;
+            out[u][v] = (coeff_t)val;
         }
     }
 }
@@ -69,68 +70,70 @@ extern "C" void dct_accel(
     coeff_t* outR,
     coeff_t* outG,
     coeff_t* outB,
-    int width,
-    int height
+    int width,   // Accept but ignore - use hardcoded WIDTH
+    int height   // Accept but ignore - use hardcoded HEIGHT
 ) {
-#pragma HLS INTERFACE m_axi port=inR offset=slave bundle=gmem0 depth=2073600
-#pragma HLS INTERFACE m_axi port=inG offset=slave bundle=gmem1 depth=2073600
-#pragma HLS INTERFACE m_axi port=inB offset=slave bundle=gmem2 depth=2073600
-#pragma HLS INTERFACE m_axi port=outR offset=slave bundle=gmem3 depth=2073600
-#pragma HLS INTERFACE m_axi port=outG offset=slave bundle=gmem4 depth=2073600
-#pragma HLS INTERFACE m_axi port=outB offset=slave bundle=gmem5 depth=2073600
+// Interface compatible with standard host
+#pragma HLS INTERFACE m_axi port=inR offset=slave bundle=gmem0
+#pragma HLS INTERFACE m_axi port=inG offset=slave bundle=gmem1
+#pragma HLS INTERFACE m_axi port=inB offset=slave bundle=gmem2
+#pragma HLS INTERFACE m_axi port=outR offset=slave bundle=gmem3
+#pragma HLS INTERFACE m_axi port=outG offset=slave bundle=gmem4
+#pragma HLS INTERFACE m_axi port=outB offset=slave bundle=gmem5
 #pragma HLS INTERFACE s_axilite port=width
 #pragma HLS INTERFACE s_axilite port=height
 #pragma HLS INTERFACE s_axilite port=return
 
-    pixel_t R_blk[8][8], G_blk[8][8], B_blk[8][8];
-#pragma HLS ARRAY_PARTITION variable=R_blk complete dim=0
-#pragma HLS ARRAY_PARTITION variable=G_blk complete dim=0
-#pragma HLS ARRAY_PARTITION variable=B_blk complete dim=0
+    // Use hardcoded dimensions for optimization
+    const int w = WIDTH;
+    const int h = HEIGHT;
 
-    coeff_t R_coef[8][8], G_coef[8][8], B_coef[8][8];
-#pragma HLS ARRAY_PARTITION variable=R_coef complete dim=0
-#pragma HLS ARRAY_PARTITION variable=G_coef complete dim=0
-#pragma HLS ARRAY_PARTITION variable=B_coef complete dim=0
+    for (int by = 0; by < h; by += 8) {
+        for (int bx = 0; bx < w; bx += 8) {
+#pragma HLS PIPELINE II=3
 
-BLOCK_Y:
-    for (int by = 0; by < height; by += 8) {
-    BLOCK_X:
-        for (int bx = 0; bx < width; bx += 8) {
-#pragma HLS PIPELINE II=1
+            pixel_t blk_R[8][8], blk_G[8][8], blk_B[8][8];
+            coeff_t coef_R[8][8], coef_G[8][8], coef_B[8][8];
 
-        LOAD_BLOCK:
+#pragma HLS ARRAY_PARTITION variable=blk_R complete dim=0
+#pragma HLS ARRAY_PARTITION variable=blk_G complete dim=0
+#pragma HLS ARRAY_PARTITION variable=blk_B complete dim=0
+#pragma HLS ARRAY_PARTITION variable=coef_R complete dim=0
+#pragma HLS ARRAY_PARTITION variable=coef_G complete dim=0
+#pragma HLS ARRAY_PARTITION variable=coef_B complete dim=0
+
+            // Load block
             for (int y = 0; y < 8; y++) {
+#pragma HLS UNROLL
                 for (int x = 0; x < 8; x++) {
+#pragma HLS UNROLL
                     int gx = bx + x;
                     int gy = by + y;
-                    if (gx < width && gy < height) {
-                        int idx = gy * width + gx;
-                        R_blk[y][x] = inR[idx];
-                        G_blk[y][x] = inG[idx];
-                        B_blk[y][x] = inB[idx];
-                    } else {
-                        R_blk[y][x] = 0;
-                        G_blk[y][x] = 0;
-                        B_blk[y][x] = 0;
-                    }
+                    int idx = gy * w + gx;
+
+                    blk_R[y][x] = inR[idx];
+                    blk_G[y][x] = inG[idx];
+                    blk_B[y][x] = inB[idx];
                 }
             }
 
-            dct_2d(R_blk, R_coef);
-            dct_2d(G_blk, G_coef);
-            dct_2d(B_blk, B_coef);
+            // Compute DCT
+            dct_2d(blk_R, coef_R);
+            dct_2d(blk_G, coef_G);
+            dct_2d(blk_B, coef_B);
 
-        STORE_BLOCK:
+            // Store coefficients
             for (int y = 0; y < 8; y++) {
+#pragma HLS UNROLL
                 for (int x = 0; x < 8; x++) {
+#pragma HLS UNROLL
                     int gx = bx + x;
                     int gy = by + y;
-                    if (gx < width && gy < height) {
-                        int idx = gy * width + gx;
-                        outR[idx] = R_coef[x][y];
-                        outG[idx] = G_coef[x][y];
-                        outB[idx] = B_coef[x][y];
-                    }
+                    int idx = gy * w + gx;
+
+                    outR[idx] = coef_R[y][x];
+                    outG[idx] = coef_G[y][x];
+                    outB[idx] = coef_B[y][x];
                 }
             }
         }
